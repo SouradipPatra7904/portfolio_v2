76a05e999cc450a15ca38ffc182d6bb52d173791
Souradip Patra <spectralphoenix.dev@gmail.com>
2025-10-08 20:23:54 +0530

    Level 4

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCodeBranch, faExternalLinkAlt, faLaptopCode, faLayerGroup,} from "@fortawesome/free-solid-svg-icons";
 import { techIconMap } from "../icons/TechIcons";
+import "../styles/Projects.css";
 
 const ProjectCard = ({ project }) => {
   return (
     <div className="project-card">
-      <h3>{project.title}</h3>
-      <p className="description">{project.description}</p>
+      <h3 className="project-title">
+        <FontAwesomeIcon icon={faLaptopCode} className="project-title-icon" />
+        {project.title}
+      </h3>
+
+      <p className="description">
+        <FontAwesomeIcon icon={faLayerGroup} className="desc-icon" />
+        {project.description}
+      </p>
 
       <div className="tech-stack">
         {project.tech.map((t, i) => (
@@ -16,14 +25,27 @@ const ProjectCard = ({ project }) => {
         ))}
       </div>
 
-      <a
-        href={project.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="github-link"
-      >
-        View on GitHub →
-      </a>
+      <div className="project-links">
+        <a
+          href={project.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          <FontAwesomeIcon icon={faCodeBranch} /> Code
+        </a>
+
+        {project.demo && (
+          <a
+            href={project.demo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="live-link"
+          >
+            <FontAwesomeIcon icon={faExternalLinkAlt} /> Live
+          </a>
+        )}
+      </div>
     </div>
   );
 };
